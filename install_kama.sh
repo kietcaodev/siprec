7a0c96954d17dbf9cda9c8093267f83aa87190fa
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2023-10-30 11:12:00 +0700

    Create install_kama.sh

---
+++ install_kama.sh
@@ -0,0 +1,11 @@
+yum -y install yum-utils gdb wget net-tools
+sudo wget -O /etc/yum.repos.d/kamailio.repo http://download.opensuse.org/repositories/home:/kamailio:/v5.6.x-rpms/CentOS_7/home:kamailio:v5.6.x-rpms.repo
+sudo yum install vim kamailio kamailio-presence kamailio-ldap kamailio-mysql kamailio-debuginfo kamailio-xmpp kamailio-unixodbc kamailio-utils kamailio-tls kamailio-outbound kamailio-gzcompress -y
+kamailio --version
+echo '[irontec]
+name=Irontec RPMs repository
+baseurl=http://packages.irontec.com/centos/$releasever/$basearch/' > /etc/yum.repos.d/irontec.repo
+rpm --import http://packages.irontec.com/public.key
+yum install sngrep -y
+cp -r /etc/kamailio /etc/kamailio.bk
+vi /etc/kamailio/kamctlrc
